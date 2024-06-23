7d60f6fd53f5db6250678d22503676f97b378c86
Hulk Choi <doksul23@gmail.com>
2024-06-23 15:10:10 +0900

    schedule page added

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,35 +1,36 @@
-'use client'
-import { useState } from "react";
-
+import Image from "next/image"
 import Link from "next/link"
 import {
-  Activity,
-  ArrowUpRight,
-  CircleUser,
-  CreditCard,
-  DollarSign,
-  Menu,
+  File,
+  Home,
+  LineChart,
+  ListFilter,
+  MoreHorizontal,
+  Package,
   Package2,
+  PanelLeft,
+  PlusCircle,
   Search,
-  Users,
+  Settings,
+  ShoppingCart,
+  Users2,
+  Menu,
+  CircleUser,
+  ArrowUpRight,
 } from "lucide-react"
 
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
-import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import {
   DropdownMenu,
+  DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
@@ -47,14 +48,48 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
-import NaverMap from "@/components/naver-map";
+export default function Schedule() {
 
-import { Database } from "@/supabase/types";
-type BadmintonClub = Database['public']['Tables']['badminton_clubs']['Row'];
+  type LinkButtonProps = {
+    children: React.ReactNode,
+    status: String // 부모컴포넌트에서 import 해온 타입을 재사용 해 줍시다.
+  }
+  const LinkButton = (props: LinkButtonProps) => {
+    const { status } = props;
+    switch (status){
+      case 'C' :
+        return (<Button disabled size="sm" className="ml-auto gap-1 w-[100px]">마감</Button>)
+      case 'customer' :
+        return (<Button asChild  size="sm" className="ml-auto gap-1 w-[100px]">
+                      
+          <Link href="#" prefetch={false}>
+            신청하기
+            <ArrowUpRight className="h-4 w-4" />
+          </Link>
+        </Button>)
+      default : 
+        return (<Button asChild size="sm" className="ml-auto gap-1 w-[100px]">
+                      
+          <Link href="#" prefetch={false}>
+            신청하기
+            <ArrowUpRight className="h-4 w-4" />
+          </Link>
+        </Button>)
+    }
+  }
 
-export default function Dashboard() {
-  const [badmintonClubs, setBadmintonClubs] = useState<BadmintonClub[]>([]);
-
+  const reserveList = [
+    {id : 0 , time : '14:00', title :'test' ,  subTitle : 'sub title', status: 'A'},
+    {id : 1 , time : '14:00', title :'test2' , subTitle : 'sub title', status: 'C'},
+    {id : 2 , time : '14:00', title :'test2' , subTitle : 'sub title', status: 'A'},
+    {id : 3 , time : '14:00', title :'test2' , subTitle : 'sub title', status: 'C'},
+    {id : 4 , time : '14:00', title :'test2' , subTitle : 'sub title', status: 'A'},
+    {id : 5 , time : '14:00', title :'test2' , subTitle : 'sub title', status: 'A'},
+    {id : 6 , time : '14:00', title :'test2' , subTitle : 'sub title', status: 'A'},
+    {id : 7 , time : '14:00', title :'test2' , subTitle : 'sub title', status: 'A'},
+    {id : 8 , time : '14:00', title :'test2' , subTitle : 'sub title', status: 'A'},
+  ]
+  
   return (
     <div className="flex min-h-screen w-full flex-col">
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
@@ -73,13 +108,13 @@ export default function Dashboard() {
             Dashboard
           </Link>
           <Link
-            href="/schedule"
+            href="#"
             className="text-muted-foreground transition-colors hover:text-foreground"
           >
-            일정
+            Orders
           </Link>
           <Link
-            href="/schedule"
+            href="#"
             className="text-muted-foreground transition-colors hover:text-foreground"
           >
             Products
@@ -117,14 +152,14 @@ export default function Dashboard() {
                 <Package2 className="h-6 w-6" />
                 <span className="sr-only">Acme Inc</span>
               </Link>
-              <Link href="#" className="hover:text-foreground">
+              <Link href="/dashboard" className="hover:text-foreground">
                 Dashboard
               </Link>
               <Link
-                href="#"
+                href="/schedule"
                 className="text-muted-foreground hover:text-foreground"
               >
-                Orders
+                일정
               </Link>
               <Link
                 href="#"
@@ -177,53 +212,49 @@ export default function Dashboard() {
         </div>
       </header>
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-          <Card className="hidden lg:block"
-             x-chunk="dashboard-01-chunk-4"
-          >
+        <div className="">
+          <Card className="xl:col-span-2" x-chunk="dashboard-01-chunk-4">
             <CardHeader className="flex flex-row items-center">
               <div className="grid gap-2">
-                <CardTitle>Infomation</CardTitle>
-                <CardDescription>
-                  체육관 정보
-                </CardDescription>
+                <CardTitle>모임</CardTitle>
+                <CardDescription>Recent transactions from your store.</CardDescription>
               </div>
               <Button asChild size="sm" className="ml-auto gap-1">
-                <Link href="#">
-                  View All
+                <Link href="/schedule/create" prefetch={false}>
+                  만들기
                   <ArrowUpRight className="h-4 w-4" />
                 </Link>
               </Button>
             </CardHeader>
-            <CardContent className="min-h-96 max-h-96 overflow-y-auto">
+            <CardContent>
               <Table>
                 <TableHeader>
                   <TableRow>
-                    <TableHead>체육관</TableHead>
-                    <TableHead>클럽</TableHead>
+                    <TableHead className="w-[50px]">시간</TableHead>
+                    <TableHead>타이틀</TableHead>
+                    <TableHead className="text-right">신청</TableHead>
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                {badmintonClubs.map((club) => (
-                  <TableRow key={club.id}>
+                {
+                  reserveList.map(reserve => (
+                    <TableRow key={reserve.id}>
                     <TableCell>
-                    <div className="font-medium">
-                      <Badge>{club.type}</Badge>{club.name}
-                    </div>
-                    <div className="hidden text-sm text-muted-foreground md:inline">
-                      {club.schedule}
-                    </div>
-                    
+                      <div className="font-medium">{reserve.time}</div>
                     </TableCell>
-                    <TableCell>{club.club_name}</TableCell>
+                    <TableCell>
+                      <div className="font-medium">{reserve.title}</div>
+                      <div className="text-sm text-muted-foreground">{reserve.subTitle}</div>
+                    </TableCell>
+                    <TableCell className="text-right">
+                      <LinkButton status={reserve.status}> </LinkButton>
+                    </TableCell>
                   </TableRow>
-                ))}
+                  ))
+                }
                 </TableBody>
               </Table>
             </CardContent>
-          </Card>
-          <Card className="xl:col-span-2 h-[500px]" x-chunk="dashboard-01-chunk-5">
-            <NaverMap onBadmintonClubFetched={setBadmintonClubs}></NaverMap>
           </Card>
         </div>
       </main>
