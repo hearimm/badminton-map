0ee71d2c03c0e07fbfd35975eee01157dded7c3c
Hulk Choi <doksul23@gmail.com>
2024-06-28 13:40:01 +0900

    feat: :sparkles: checkAuth change login/logout button
    when login show logout button and when logout show login button

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from "next/link"
 import {
   Package2,
@@ -5,7 +6,6 @@ import {
   Menu,
   CircleUser,
 } from "lucide-react"
-
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
@@ -17,78 +17,100 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import { useRouter } from 'next/navigation';
+import { createClient } from "@/utils/supabase/client"  // Import Supabase client
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  const router = useRouter()
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const supabase = createClient()
+      const { data } = await supabase.auth.getSession()
+      console.log(data);
+      setIsAuthenticated(!!data?.session)
+    }
+    checkAuth()
+  }, [])
+
+  const handleLogout = async () => {
+    const supabase = createClient();
+    const { error } = await supabase.auth.signOut()
+    if (error) {
+      console.error("Error logging out:", error)
+    } else {
+      router.push('/login')  // Redirect to the login page after logging out
+    }
+  }
+
+  const handleLogin = async () => {
+    router.push('/login')  // Redirect to the login page
+  }
 
   const linkList = [
-    {href: '/dashboard', name: 'Dashboard'},
-    {href: '/schedule', name: 'Schedule'},
-    {href: '/map', name: 'Map'},
+    { href: '/dashboard', name: 'Dashboard' },
+    { href: '/schedule', name: 'Schedule' },
+    { href: '/map', name: 'Map' },
   ]
 
   return (
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-          <Link
-            href="#"
-            className="flex items-center gap-2 text-lg font-semibold md:text-base"
-          >
-            <Package2 className="h-6 w-6" />
-            <span className="sr-only">Acme Inc</span>
+      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <Link
+          href="#"
+          className="flex items-center gap-2 text-lg font-semibold md:text-base"
+        >
+          <Package2 className="h-6 w-6" />
+          <span className="sr-only">Acme Inc</span>
+        </Link>
+        {linkList.map(e => (
+          <Link key={e.name} href={e.href} className="text-foreground transition-colors hover:text-foreground">
+            {e.name}
           </Link>
-          {
-            linkList.map( e => {
-              return (
-                <Link key={e.name} href={e.href} className="text-foreground transition-colors hover:text-foreground">
-                  {e.name}
-                </Link>
-              )
-            })
-          }
-        </nav>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button
-              variant="outline"
-              size="icon"
-              className="shrink-0 md:hidden"
+        ))}
+      </nav>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
+            className="shrink-0 md:hidden"
+          >
+            <Menu className="h-5 w-5" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav className="grid gap-6 text-lg font-medium">
+            <Link
+              href="#"
+              className="flex items-center gap-2 text-lg font-semibold"
             >
-              <Menu className="h-5 w-5" />
-              <span className="sr-only">Toggle navigation menu</span>
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="left">
-            <nav className="grid gap-6 text-lg font-medium">
-              <Link
-                href="#"
-                className="flex items-center gap-2 text-lg font-semibold"
-              >
-                <Package2 className="h-6 w-6" />
-                <span className="sr-only">Acme Inc</span>
+              <Package2 className="h-6 w-6" />
+              <span className="sr-only">Acme Inc</span>
+            </Link>
+            {linkList.map(e => (
+              <Link key={e.name} href={e.href} className="hover:text-foreground">
+                {e.name}
               </Link>
-              {
-                linkList.map( e => {
-                  return (
-                    <Link key={e.name} href={e.href} className="hover:text-foreground">
-                      {e.name}
-                    </Link>
-                  )
-                })
-              }
-            </nav>
-          </SheetContent>
-        </Sheet>
-        <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-          <form className="ml-auto flex-1 sm:flex-initial">
-            <div className="relative">
-              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type="search"
-                placeholder="Search products..."
-                className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-              />
-            </div>
-          </form>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <form className="ml-auto flex-1 sm:flex-initial">
+          <div className="relative">
+            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              type="search"
+              placeholder="Search products..."
+              className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+            />
+          </div>
+        </form>
+        {isAuthenticated ? (
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="secondary" size="icon" className="rounded-full">
@@ -102,10 +124,16 @@ export default function Header() {
               <DropdownMenuItem>Settings</DropdownMenuItem>
               <DropdownMenuItem>Support</DropdownMenuItem>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
+              <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-        </div>
+        ) : (
+          <Button variant="secondary" size="icon" className="rounded-full" onClick={handleLogin}>
+            <CircleUser className="h-5 w-5" />
+            <span className="sr-only">Login</span>
+          </Button>
+        )}
+      </div>
     </header>
   )
 }
