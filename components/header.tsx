dfb61865e1550876c487726de0059aefe3ddf33d
Hulk Choi <doksul23@gmail.com>
2024-07-08 16:36:14 +0900

    refactor(components/header): update header styling and functionality
    - Consolidated import statements for improved readability.
    - Changed `Package2` icon to `FeatherIcon`.
    - Adjusted `linkList` item name from 'Schedule' to 'Match'.
    - Added z-index to the header for better stacking context.
    - Improved authentication check and logout handling.

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,7 @@
-'use client'
-import Link from "next/link"
-import {
-  Package2,
-  Search,
-  Menu,
-  CircleUser,
-} from "lucide-react"
-import { Button } from "@/components/ui/button"
+'use client';
+import Link from "next/link";
+import { Search, Menu, CircleUser, FeatherIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -14,60 +9,60 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Input } from "@/components/ui/input"
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { useRouter } from 'next/navigation';
-import { createClient } from "@/utils/supabase/client"  // Import Supabase client
+import { createClient } from "@/utils/supabase/client";  // Import Supabase client
 import { useEffect, useState } from 'react';
 
 export default function Header() {
-  const router = useRouter()
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
     const checkAuth = async () => {
-      const supabase = createClient()
-      const { data } = await supabase.auth.getSession()
+      const supabase = createClient();
+      const { data } = await supabase.auth.getSession();
       console.log(data);
-      setIsAuthenticated(!!data?.session)
-    }
-    checkAuth()
-  }, [])
+      setIsAuthenticated(!!data?.session);
+    };
+    checkAuth();
+  }, []);
 
   const handleLogout = async () => {
     const supabase = createClient();
-    const { error } = await supabase.auth.signOut()
+    const { error } = await supabase.auth.signOut();
     if (error) {
-      console.error("Error logging out:", error)
+      console.error("Error logging out:", error);
     } else {
-      router.push('/login')  // Redirect to the login page after logging out
+      router.push('/login');  // Redirect to the login page after logging out
     }
-  }
+  };
 
   const handleProfile = () => {
-    router.push('/profile')  // Redirect to profile page
-  }
+    router.push('/profile');  // Redirect to profile page
+  };
   
   const handleLogin = async () => {
-    router.push('/login')  // Redirect to the login page
-  }
+    router.push('/login');  // Redirect to the login page
+  };
 
   const linkList = [
     { href: '/dashboard', name: 'Dashboard' },
-    { href: '/match', name: 'Schedule' },
+    { href: '/match', name: 'Match' },
     { href: '/map', name: 'Map' },
-  ]
+  ];
 
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+    <header className="sticky z-50 top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
           href="#"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
-          <Package2 className="h-6 w-6" />
-          <span className="sr-only">Acme Inc</span>
+          <FeatherIcon className="h-6 w-6" />
+          <span className="sr-only">R.ally</span>
         </Link>
         {linkList.map(e => (
           <Link key={e.name} href={e.href} className="text-foreground transition-colors hover:text-foreground">
@@ -92,8 +87,8 @@ export default function Header() {
               href="#"
               className="flex items-center gap-2 text-lg font-semibold"
             >
-              <Package2 className="h-6 w-6" />
-              <span className="sr-only">Acme Inc</span>
+              <FeatherIcon className="h-6 w-6" />
+              <span className="sr-only">R.ally</span>
             </Link>
             {linkList.map(e => (
               <Link key={e.name} href={e.href} className="hover:text-foreground">
